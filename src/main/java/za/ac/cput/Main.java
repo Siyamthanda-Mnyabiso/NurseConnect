c6ebbd085a4c98d359b888ab69b978f111bc5e10
Siyamthanda Mnyabiso <106501405+SiyaModdy@users.noreply.github.com>
2026-03-18 15:20:44 +0200

    Just finished building the factory and helper class

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,9 +1,13 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.Nurse;
+import za.ac.cput.factory.NurseFactory;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the main ");
+        Nurse nurse = NurseFactory.createNurse("999006789", "Sipho", "Maxoli", "sipho_m@gmail.com", "0732967899", "Shop 4, Grand Parade Centre, 11 Adderley St, Foreshore, Cape Town, 8001", "Nursing Science", "Critical Care/ICU Nurse", "5 Years", true, "RN079078");
+        System.out.println(nurse);
     }
 }
---
+++ src/main/java/za/ac/cput/factory/NurseFactory.java
@@ -0,0 +1,42 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Nurse;
+import za.ac.cput.util.Helper;
+
+public class NurseFactory {
+    public static Nurse createNurse (String NurseID, String FirstName, String LastName, String Email, String PhoneNumber, String Address, String Qualification, String Specialization, String YearsOfExperience, boolean AvailabilityStatus, String LicenseNumber ){
+        if (Helper.isNullOrEmpty(NurseID) || Helper.isNullOrEmpty(FirstName)|| Helper.isNullOrEmpty(LastName) || Helper.isNullOrEmpty(Address) ||  Helper.isNullOrEmpty(Qualification) || Helper.isNullOrEmpty(Specialization) || Helper.isNullOrEmpty(YearsOfExperience)){
+            return null;
+        }
+
+        if (!Helper.isValidEmail(Email)){
+            return null;
+        }
+
+
+
+        if (!Helper.isValidPhoneNumber(PhoneNumber)){
+            return null;
+        }
+
+        if (!Helper.isValidLicenseNum(LicenseNumber)){
+            return null;
+        }
+
+
+
+        return new Nurse.Builder()
+                .setNurseID(NurseID)
+                .setAddress(Address)
+                .setFirstName(FirstName)
+                .setLastName(LastName)
+                .setEmail(Email)
+                .setQualification(Qualification)
+                .setSpecialization(Specialization)
+                .setYearsOfExperience(YearsOfExperience)
+                .setLicenseNumber(LicenseNumber)
+                .setPhoneNumber(PhoneNumber)
+                .setAvailabilityStatus(AvailabilityStatus)
+                .build();
+    }
+}
