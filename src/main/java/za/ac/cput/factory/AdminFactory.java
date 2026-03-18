93a307919896bed5b2f94a0ee98d0d8e73029e53
Mangaliso <241177049@mycput.ac.za>
2026-03-18 17:15:59 +0200

    Added a new admin factory class

---
+++ src/main/java/za/ac/cput/factory/AdminFactory.java
@@ -0,0 +1,32 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Admin;
+import za.ac.cput.util.Helper;
+
+public class AdminFactory {
+    public static Admin createAdmin(String id, String name, String email, String role, String password){
+        if(Helper.isIdValid(id)){
+            return null;
+        }
+        if(Helper.isNullOrEmpty(name)){
+            return null;
+        }
+        if(Helper.isEmailValid(email)){
+            return null;
+        }
+        if(Helper.isAdminRoleValid(role)){
+            return null;
+        }
+        if(Helper.isPasswordValid(password)){
+            return null;
+        }
+        return new Admin.Builder()
+                .setAdminId(id)
+                .setAdminName(name)
+                .setAdminEmail(email)
+                .setAdminRole(role)
+                .setPassword(password)
+                .build();
+    }
+
+}
