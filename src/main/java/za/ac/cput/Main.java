8bfddd64babe88fe28dfae027ef24917755c9f2a
luxolo <@luxolo98>
2026-03-26 20:56:31 +0200

    resolved merge conflict in Main.java

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -9,15 +9,13 @@ import za.ac.cput.factory.NurseFactory;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
         Nurse nurse = NurseFactory.createNurse("999006789", "Sipho", "Maxoli", "sipho_m@gmail.com", "0732967899", "Shop 4, Grand Parade Centre, 11 Adderley St, Foreshore, Cape Town, 8001", "Nursing Science", "Critical Care/ICU Nurse", "5 Years", true, "RN079078");
         System.out.println(nurse);
 
         Admin admin = AdminFactory.createAdmin("23765434567","Admin new","admin@gmail.com","Admin","Gee124##") ;
         System.out.println(admin);
->>>>>>> acf405f66f5fcaff798c7abc684a36907ae828b0
+
     }
 
 }
