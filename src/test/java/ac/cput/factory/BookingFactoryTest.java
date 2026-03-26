cb8e2856bb5515d6adeea69160b260d84adb9470
Mvuyisi <MvuyisiLindo@gmail.com>
2026-03-27 00:47:03 +0200

    Booking Repository test and Booking Factory Test

--- src/test/java/ac/cput/factory/BookingFactoryTest.java
+++ src/test/java/ac/cput/factory/BookingFactoryTest.java
@@ -1,15 +1,30 @@
 package ac.cput.factory;
 
 import ac.cput.domain.Booking;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class BookingFactoryTest {
 
-    @org.junit.jupiter.api.Test
-    void createBooking() {
-        Booking book = new BookingFactory.createBooking("676888","76869","03 March 2026","Available");
-        assertNotNull(book);
-        System.out.println(book);
+    private static Booking B1, B2;
+
+    @BeforeEach
+    void setUp(){
+        B1 = BookingFactory.createBooking("5677","676w8","4 March 2025","Available");
+        B2 = BookingFactory.createBooking("","7348348","5 May 2026","Not Available");
+    }
+    @Test
+    void a_testBooking(){
+        assertNotNull(B1);
+        System.out.println(B1.toString());
+    }
+    @Test
+    void b_testBookingFails(){
+        assertNotNull(B2);
+        System.out.println(B2.toString());
     }
 }
